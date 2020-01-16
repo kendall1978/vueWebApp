088acbf2dee28cee53002412b3a56a2150c8e6e9
kendall1978 <46752584+kendall1978@users.noreply.github.com>
2020-01-16 10:26:08 -0600

    1/16/2020

--- src/API.js
+++ src/API.js
@@ -6,15 +6,5 @@ new Vue({
             info: null
         }
     },
-    mounted(){
-        axios
-        .get('https://rawg-video-games-database.p.rapidapi.com/developers/%7Bid%7D')
-        .then(response => (this.info = response.data))
-        .headers({
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-            "x-rapidapi-key": "d7727d5987msh950787fe21acfc7p1ed9d9jsn957ca8def9ae"
-            })
-        .catch(err => (this.info = err))
-    }
+
 })
--- src/components/projects/projects.vue
+++ src/components/projects/projects.vue
@@ -1,21 +1,39 @@
 <template>
   <div>
-    <b-card-group columns>
+    <b-card-group style="max-width: 100vw; display: flex; flex-direction: row;
+    flex-wrap: wrap; justify-content: space-evenly; align-items: flex-start;
+    align-content: flex-start;">
       <b-card 
-        title="IGDB API call"
-        img-src="https://theblogaboutwhatever.files.wordpress.com/2018/08/igdb.jpg"
+        title="The Movie DB API call"
+        img-src="https://pbs.twimg.com/profile_images/789117657714831361/zGfknUu8_400x400.jpg"
         img-alt="Image"
         img-top
+        style="width: 40%; display: flex; flex-direction: column;
+        max-height: 100vh;"
         id="blogCard"
       >
         <b-card-text>
-          With this component I will display some small cards that show some 
-          information I grabbed from the IGDB website using an API call. 
-          I am also trying out axios for the first time, so don't judge.
-          Hit the button below to try it out.
+          In this project I am making a call to the Movie DB and collecting 
+          on around 200 or so movies. From there then im practicing some techniques 
+          that you might see in other movie information websites.
         </b-card-text>
-        <b-card-text class="small text-muted">Last updated 01/08/2020 </b-card-text>
-        <b-button to="/apiExample">Button</b-button>
+        <b-button to="/apiExample">Movies</b-button>
+      </b-card>
+
+      <b-card 
+        title="Pong in Python"
+        img-src="http://pong-2.com/icon-256.png"
+        img-alt="Image"
+        img-top
+        style="width: 40%; display: flex; flex-direction: column;"
+        id="blogCard"
+      >
+        <b-card-text>
+          In this project I am making a call to the Movie DB and collecting 
+          on around 200 or so movies. From there then im practicing some techniques 
+          that you might see in other movie information websites.
+        </b-card-text>
+        <b-button to="/apiExample">Movies</b-button>
       </b-card>
     </b-card-group>
   </div>
